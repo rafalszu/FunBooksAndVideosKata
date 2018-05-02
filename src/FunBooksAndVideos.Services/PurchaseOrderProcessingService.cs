8c2a01439689162e6016c91170ee674fef0ee635
Rafal Szumilas <rafal@szumilas.org>
2018-05-03 01:49:34 +0200

    order processing service, work in progress

--- src/FunBooksAndVideos.Services/PurchaseOrderProcessingService.cs
+++ src/FunBooksAndVideos.Services/PurchaseOrderProcessingService.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections.Concurrent;
+using System.Linq;
 using System.Threading.Tasks;
 using FunBooksAndVideos.Models;
 
 namespace FunBooksAndVideos.Services
 {
+    // in a real world scenario this would be done using some messaging system like rabbitmq
+    // and subscribers would be responsible for processing purhcase orders
     public class PurchaseOrderProcessingService : IPurchaseOrderProcessingService
     {
         private ConcurrentQueue<PurchaseOrder> _orderQueue;
@@ -18,11 +22,39 @@ namespace FunBooksAndVideos.Services
             }
         }
 
-        public Task ProcessPurchaseOrderAsync(PurchaseOrder purchaseOrder)
+        public async Task ProcessPurchaseOrderAsync(PurchaseOrder purchaseOrder)
         {
-            return Task.Factory.StartNew(() => {
-                _orderQueue.Enqueue(purchaseOrder);
-            });
+            if(purchaseOrder == null)
+                throw new ArgumentNullException(nameof(purchaseOrder));
+
+            // schedule queue processing after 2 seconds
+            await Task.Factory.StartNew(async () => await ProcessItemsInQueueAsync(2000));
+
+            _orderQueue.Enqueue(purchaseOrder);
+        }
+
+        private async Task ProcessItemsInQueueAsync(int delayMs)
+        {
+            await Task.Delay(delayMs);
+            
+            // for the sake of simplicity this is just a simple loop, but should be parallelized
+            while(OrderQueue.Count > 0)
+            {
+                PurchaseOrder order = null;
+                if(OrderQueue.TryDequeue(out order))
+                    await FinalizePurchaseOrderAsync(order);
+            }
+        }
+
+        private async Task FinalizePurchaseOrderAsync(PurchaseOrder order)
+        {
+            if(order == null)
+                throw new ArgumentNullException(nameof(order));
+
+            // get all assemblies with interface IOrderProcessor
+            // go line by line
+            // check if any of assemblies CanProcess given productType
+            // if so, process line
         }
     }
 }
